5adafc2520bc3534c96d0a9dfc141af2eac6ebd5
Andrey Kostyuk <jardinesdelareina@mail.ru>
2022-05-22 20:08:44 +0300

    введение в сериализацию

--- blog/serializers.py
+++ blog/serializers.py
@@ -1,8 +1,10 @@
 from rest_framework import serializers
-from .models import Blog
 
 
-class BlogSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Blog
-        fields = ('title', 'category')
+class BlogSerializer(serializers.Serializer):
+    title = serializers.CharField(max_length=50)
+    text = serializers.CharField(max_length=2000)
+    created_at = serializers.DateTimeField(read_only=True)
+    updated_at = serializers.DateTimeField(read_only=True)
+    is_published = serializers.BooleanField(default=True)
+    category = serializers.CharField()
--- blog/views.py
+++ blog/views.py
@@ -1,12 +1,26 @@
 from django.shortcuts import render
-from rest_framework import generics
+from rest_framework.views import APIView
+from rest_framework.response import Response
 from .serializers import BlogSerializer
 from .models import Blog
 
 # Create your views here.
 
 
-class BlogAPIView(generics.ListAPIView):
-    queryset = Blog.objects.all()
-    serializer_class = BlogSerializer
+class BlogAPIView(APIView):
+    def get(self, request):
+        blog = Blog.objects.all()
+        return Response({'notes': BlogSerializer(blog, many=True).data})
+
+    def post(self, request):
+        serializer = BlogSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+
+        note_new = Blog.objects.create(
+            title=request.data['title'],
+            text=request.data['text'],
+            category=request.data['category']
+        )
+
+        return Response({'note': BlogSerializer(note_new).data})
     
