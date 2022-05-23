9b8b1a72d1d47e5983455a9568fe8a08d1c5f967
Andrey Kostyuk <jardinesdelareina@mail.ru>
2022-05-24 00:09:01 +0300

    авторизация по cookie

--- myapp/urls.py
+++ myapp/urls.py
@@ -8,7 +8,8 @@ router.register(r'blog', BlogViewSet) '''
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    #path('api/v1/', include(router.urls))   # http://127.0.0.1:8000/api/v1/blog/
+    #path('api/v1/blog-auth/', include('rest_framefowk.urls'))  # Авторизация на основе сессий (cookies)  
+    #path('api/v1/', include(router.urls))                      # http://127.0.0.1:8000/api/v1/blog/
     path('api/v1/blog/', BlogAPIList.as_view()),
     path('api/v1/blog/<int:pk>', BlogAPIUpdate.as_view()),
     path('api/v1/blogdelete/<int:pk>', BlogAPIDestroy.as_view()),
