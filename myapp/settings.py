ea192225078e144465fb0b57988b76ae4143ba25
Andrey Kostyuk <jardinesdelareina@mail.ru>
2022-05-25 00:31:32 +0300

    jwt

--- myapp/settings.py
+++ myapp/settings.py
@@ -1,3 +1,4 @@
+from datetime import timedelta
 from pathlib import Path
 
 from django.db import DEFAULT_DB_ALIAS
@@ -129,8 +130,44 @@ REST_FRAMEWORK = {
     ],
 
     'DEFAULT_AUTHENTICATION_CLASSES': [
-        'rest_framework.authentication.TokenAuthentication',     # Аутентификация по токенам
+        #'rest_framework.authentication.TokenAuthentication',     # Аутентификация по токенам
+        'rest_framework.authentication.TokenAuthentication',     # Аутентификация по jwt токенам
         'rest_framework.authentication.BasicAuthentication',     # Аутентификация по сессиям (есть по умолчанию)
         'rest_framework.authentication.SessionAuthentication',   # Аутентификация по сессиям (есть по умолчанию)
     ]
+}
+
+
+# Настройки JWT 
+
+SIMPLE_JWT = {
+    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=5),
+    'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
+    'ROTATE_REFRESH_TOKENS': False,
+    'BLACKLIST_AFTER_ROTATION': False,
+    'UPDATE_LAST_LOGIN': False,
+
+    'ALGORITHM': 'HS256',
+    'SIGNING_KEY': SECRET_KEY,
+    'VERIFYING_KEY': None,
+    'AUDIENCE': None,
+    'ISSUER': None,
+    'JWK_URL': None,
+    'LEEWAY': 0,
+
+    'AUTH_HEADER_TYPES': ('Bearer',),           # Это слово должно фигурировать перед JWT-токеном при авторизации
+    'AUTH_HEADER_NAME': 'HTTP_AUTHORIZATION',
+    'USER_ID_FIELD': 'id',
+    'USER_ID_CLAIM': 'user_id',
+    'USER_AUTHENTICATION_RULE': 'rest_framework_simplejwt.authentication.default_user_authentication_rule',
+
+    'AUTH_TOKEN_CLASSES': ('rest_framework_simplejwt.tokens.AccessToken',),
+    'TOKEN_TYPE_CLAIM': 'token_type',
+    'TOKEN_USER_CLASS': 'rest_framework_simplejwt.models.TokenUser',
+
+    'JTI_CLAIM': 'jti',
+
+    'SLIDING_TOKEN_REFRESH_EXP_CLAIM': 'refresh_exp',
+    'SLIDING_TOKEN_LIFETIME': timedelta(minutes=5),
+    'SLIDING_TOKEN_REFRESH_LIFETIME': timedelta(days=1),
 }
--- myapp/urls.py
+++ myapp/urls.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.urls import path, re_path, include
 from blog.views import *
 from rest_framework import routers
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView, TokenVerifyView
 
 ''' router = routers.DefaultRouter()
 router.register(r'blog', BlogViewSet) '''
@@ -15,4 +16,7 @@ urlpatterns = [
     path('api/v1/blog/', BlogAPIList.as_view()),
     path('api/v1/blog/<int:pk>', BlogAPIUpdate.as_view()),
     path('api/v1/blogdelete/<int:pk>', BlogAPIDestroy.as_view()),
+    path('api/v1/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),   # JWT
+    path('api/v1/token/refresh/', TokenRefreshView.as_view(), name='token_refrash'),  # JWT
+    path('api/v1/token/verify', TokenVerifyView.as_view(), name='token_verify'),      # JWT
 ]
